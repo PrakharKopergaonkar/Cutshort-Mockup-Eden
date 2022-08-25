029d36c525ff3910348c1d11075c1b6d442c080d
prakhar <pkopergaonkar@gmail.com>
2022-08-25 19:29:41 +0530

    remove react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './screens/Home/Home';
 
 function App() {
   return (
     <Fragment>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-        </Routes>
-      </Router>
+      <Home/>
     </Fragment>
   );
 }
